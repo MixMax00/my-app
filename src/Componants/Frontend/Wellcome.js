9ff17dda21a0cf15212984ef1038d53da1db5551
mhrhabib <mmix00595@gmail.com>
2023-01-31 16:05:38 +0600

    Protected Route

---
+++ src/Componants/Frontend/Wellcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+class Wellcome extends Component {
+    render() {
+        return <div className="wellcome-page">
+                <h3>Wellcome react and laravel 9.</h3>
+        </div>; 
+    }
+}
+
+export default Wellcome;
